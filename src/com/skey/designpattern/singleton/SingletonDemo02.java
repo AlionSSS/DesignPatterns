69ef8b0ab66d0cd244cda55aa0775034504711c4
ALion <alionsss@foxmail.com>
2019-03-05 11:08:02 +0800

    单例设计模式

--- src/com/skey/designpattern/singleton/SingletonDemo02.java
+++ src/com/skey/designpattern/singleton/SingletonDemo02.java
@@ -8,7 +8,12 @@ package com.skey.designpattern.singleton;
  */
 public class SingletonDemo02 {
 
-    private static SingletonDemo02 instance;
+    // volatile防止JIT优化器对指令重排序（会导致线程不安全）
+    // 原先创建顺序：1.创建对象的内存实例 2.创建对象引用
+    // JIT优化重排序后：1.创建对象引用 2.创建对象的内存实例
+    // 解释: 重排序后，优先创建了对象引用，所以instance == null会返回false，
+    // 这个时候就会直接返回对象，但是对象可能还没创建在内存的实例，因此会导致空指针
+    private volatile static SingletonDemo02 instance;
 
     // 私有，防止直接实例化（不过仍会被反射实例化）
     private SingletonDemo02() {
